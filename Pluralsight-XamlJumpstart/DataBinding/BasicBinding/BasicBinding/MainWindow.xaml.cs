6d0c58a1aa08f103c0103f81ee2a5250afba724e
AngelaHong <Angela_Hong1@dell.com>
2016-02-15 15:39:34 -0600

    2/15/2016

---
+++ Pluralsight-XamlJumpstart/DataBinding/BasicBinding/BasicBinding/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = person.Name + " " + person.Address.Number + ", " + person.Address.Street;
+            MessageBox.Show(message);
+        }
+    }
+}
---
+++ Pluralsight-XamlJumpstart/DataBinding/BasicBinding/BasicBinding/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicBinding
+{
+    public class Person
+    {
+        public string Name { get; set;}
+        public Address Address { get; set; }
+        public Person()
+        {
+            this.Address = new Address();
+        }
+    }
+}
